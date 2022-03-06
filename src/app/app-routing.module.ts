7ff07462680560ccec32c8f459611a192bbfc9c5
patorlowska <p.patorlowska@gmail.com>
2022-03-06 14:03:26 +0100

    added aboutus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageModule } from './pages/home.page-module';
+import { AboutUsPageModule } from './pages/about-us.page-module';
 
-const routes: Routes = [{ 
-        path: '', 
-        loadChildren: () => HomePageModule
-      }];
+const routes: Routes = [{
+  path: '',
+  loadChildren: () => HomePageModule
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
