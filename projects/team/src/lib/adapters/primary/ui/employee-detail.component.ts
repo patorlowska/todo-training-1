e4afcb1ae0bac1227173efced1d5cb0edddfcc48
patorlowska <p.patorlowska@gmail.com>
2022-04-12 08:52:02 +0200

    delete snaphot

--- projects/team/src/lib/adapters/primary/ui/employee-detail.component.ts
+++ projects/team/src/lib/adapters/primary/ui/employee-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, ViewEncapsulation, ChangeDetectionStrategy, Inject } from '@
 import { Observable } from 'rxjs';
 import { PersonDTO } from '../../../application/ports/secondary/person.dto';
 import { GETS_ONE_PERSON_DTO, GetsOnePersonDtoPort } from '../../../application/ports/secondary/gets-one-person.dto-port';
-import { tap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
     selector: 'lib-employee-detail',
@@ -13,10 +13,9 @@ import { tap } from 'rxjs/operators';
 })
 export class EmployeeDetailComponent {
 
-    // params$ = this._activatedRoute.params;
-    employee$: Observable<PersonDTO> = this._getsOnePersonDto.getOne(
-        this._activatedRoute.snapshot.params.personId
-    ).pipe(tap(x => console.log(x)));
+    employee$: Observable<PersonDTO> = this._activatedRoute.params
+        .pipe(switchMap(({ personId }) => this._getsOnePersonDto.getOne(personId)))
+        .pipe(tap(x => console.log(x)));
 
     constructor(
         @Inject(GETS_ONE_PERSON_DTO)
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [{
   loadChildren: () => EmployeesPageModule
 },
 {
-  path: 'pracownik',
+  path: 'pracownik/:personId',
   loadChildren: () => EmployeeDetailsPageModule
 },
 {
